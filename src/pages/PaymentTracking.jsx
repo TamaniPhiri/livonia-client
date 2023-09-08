347f2c11c7e5e342eec3de9632c6d1609b62eae0
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 22:46:54 +0200

    chnages

--- src/pages/PaymentTracking.jsx
+++ src/pages/PaymentTracking.jsx
@@ -1,26 +1,31 @@
 const PaymentTracking = () => {
   return (
     <div className="flex items-center justify-center w-full flex-col min-h-screen px-4 md:px-8 lg:px-12 py-28">
-      <h1 className="font-bold uppercase text-2xl md:text-4xl">Payment Tracking</h1>
+      <h1 className="font-bold uppercase text-2xl md:text-4xl">
+        Payment Tracking
+      </h1>
       <div className="border-4 border-gray-700 mt-4 gap-4 p-4 md:p-8 lg:p-12 rounded-3xl grid lg:grid-cols-2 grid-cols-1 items-center justify-center lg:max-w-2xl md:max-w-lg w-full bg-[#2b2b2b]">
         <div className="grid gap-2">
-          <span>
-            Client Name
-          </span>
-          <input type="text" placeholder="Name" className=" p-3 rounded-md text-black focus:outline-none"/>
+          <span>Client Name</span>
+          <input
+            type="text"
+            placeholder="Name"
+            className=" p-3 rounded-md text-black focus:outline-none"
+          />
         </div>
         <div className="grid gap-2">
-          <span>
-            Date of Purchase
-          </span>
-          <input type="date" className="text-black p-3 rounded-md focus:outline-none"/>
+          <span>Date of Purchase</span>
+          <input
+            type="date"
+            className="text-black p-3 rounded-md focus:outline-none"
+          />
         </div>
+        <h1 className="col-span-2 w-full mt-4 font-semibold text-left">
+          Product Details
+        </h1>
       </div>
-      <h1 className="col-span-2 w-full text-left">
-        Product Details
-      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default PaymentTracking
+export default PaymentTracking;
