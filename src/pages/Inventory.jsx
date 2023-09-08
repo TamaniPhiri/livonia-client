7bc266f88d650fdd6c47528c5e33f66ddd7d43ff
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 23:30:42 +0200

    chnages

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,6 +1,10 @@
 const Inventory = () => {
   return (
-    <div>Inventory</div>
+    <div className="flex items-center justify-center w-full flex-col min-h-screen px-4 md:px-8 lg:px-12 py-28">
+      <h1 className="font-bold uppercase text-2xl md:text-4xl">
+        Inventory
+      </h1>
+    </div>
   )
 }
 
