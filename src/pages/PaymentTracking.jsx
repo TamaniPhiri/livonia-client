63dd08cbafd4d015807dae7b6139898b9290cacd
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 22:38:58 +0200

    chnages

--- src/pages/PaymentTracking.jsx
+++ src/pages/PaymentTracking.jsx
@@ -1,9 +1,9 @@
 const PaymentTracking = () => {
   return (
-    <div className="flex items-center justify-center w-full px-4 md:px-8 lg:px-12 py-28">
+    <div className="flex items-center justify-center w-full flex-col min-h-screen px-4 md:px-8 lg:px-12 py-28">
       <h1 className="font-bold uppercase text-2xl md:text-4xl">Payment Tracking</h1>
-      <div>
-
+      <div className="border border-gray-700 rounded-3xl grid lg:grid-cols-2 grid-cols-1 items-center justify-center lg:max-w-2xl md:max-w-lg w-full bg-[#2b2b2b]">
+        hello
       </div>
     </div>
   )
