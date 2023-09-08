585a8c4011716545d04d5379ac96df1ca9c74952
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 22:03:45 +0200

    chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
+import Inventory from "./pages/Inventory";
+import Client from "./pages/Client";
+import PaymentTracking from "./pages/PaymentTracking";
+import Navbar from "./components/Global/Navbar";
 
 function App() {
-
   return (
     <>
-      app
+      <Navbar />
+      <Routes>
+        <Route path="/" Component={Dashboard} />
+        <Route path="/inventory" Component={Inventory} />
+        <Route path="/client" Component={Client} />
+        <Route path="/payment-tracking" Component={PaymentTracking} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
