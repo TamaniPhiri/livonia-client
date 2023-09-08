5cb02c725c1d144ed927e3926c20e0497fd3c52a
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 22:21:22 +0200

    chnages

--- src/components/Global/Navbar.jsx
+++ src/components/Global/Navbar.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="fixed top-0 w-full items-center justify-center bg-[#2b2b2b] shadow-xl">
+      <nav className="w-full flex justify-between items-center px-4 md:px-8 lg:px-12">
+        <div>1</div>
+        <div>
+          <Link>Client Management</Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
