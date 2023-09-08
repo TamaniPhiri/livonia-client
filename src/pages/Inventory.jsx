19b9f6a41d66acc910866ac118ee0551f23f7ef6
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 23:32:02 +0200

    chnages

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,11 +1,21 @@
 const Inventory = () => {
   return (
     <div className="flex items-center justify-center w-full flex-col min-h-screen px-4 md:px-8 lg:px-12 py-28">
-      <h1 className="font-bold uppercase text-2xl md:text-4xl">
-        Inventory
-      </h1>
+      <h1 className="font-bold uppercase text-2xl md:text-4xl">Inventory</h1>
+      <div className="border-4 border-gray-700 mt-4 gap-4 p-4 md:p-8 lg:p-12 rounded-3xl flex flex-col items-center justify-center lg:max-w-2xl md:max-w-lg w-full bg-[#2b2b2b]">
+        <div className="grid gap-6 lg:grid-cols-2 w-full">
+          <div className="grid gap-2">
+            <span>Client Name</span>
+            <input
+              type="text"
+              placeholder="Name"
+              className=" p-3 rounded-md text-black focus:outline-none"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inventory
+export default Inventory;
