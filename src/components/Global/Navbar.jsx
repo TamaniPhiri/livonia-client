186832837c241f5b51cd66a756a1842858876d44
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 23:23:21 +0200

    chnages

--- src/components/Global/Navbar.jsx
+++ src/components/Global/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full items-center justify-center bg-[#2b2b2b] shadow-xl">
+    <div className="fixed top-0 w-full items-center justify-center bg-opacity-50 backdrop-blur-md z-50 bg-[#2b2b2b] shadow-xl">
       <nav className="w-full flex justify-between py-6 items-center px-4 md:px-8 lg:px-12">
         <Link to={"/"} className="text-xl md:text-2xl font-bold">Livonia</Link>
         <div className="lg:flex hidden gap-8">
