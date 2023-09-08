d35b34ccc6ab00f7b7c97c27ba1e3ae485daed41
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 22:32:32 +0200

    chnages

--- src/components/Global/Navbar.jsx
+++ src/components/Global/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="fixed top-0 w-full items-center justify-center bg-[#2b2b2b] shadow-xl">
       <nav className="w-full flex justify-between py-6 items-center px-4 md:px-8 lg:px-12">
-        <Link to={"/"}></Link>
+        <Link to={"/"} className="text-xl md:text-2xl font-bold">Livonia</Link>
         <div className="lg:flex hidden gap-8">
           <Link to={"/client"}>Client Management</Link>
           <Link to={"/inventory"}>Inventory</Link>
