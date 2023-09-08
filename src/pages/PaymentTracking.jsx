f44c5d92f8bb8a26c058acd4c4c75dc3cb29aab4
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-08 23:21:25 +0200

    chnages

--- src/pages/PaymentTracking.jsx
+++ src/pages/PaymentTracking.jsx
@@ -171,6 +171,34 @@ const PaymentTracking = () => {
             />
           </div>
         </div>
+
+        <div className="grid gap-6 lg:grid-cols-2 border-b pb-6 border-gray-600 w-full">
+          {/* Tubes */}
+          <div className="grid gap-2">
+            <span>Tubes</span>
+            <input
+              type="text"
+              placeholder="Brand"
+              className=" p-3 rounded-md text-black focus:outline-none"
+            />
+          </div>
+          {/* Size and Quantity */}
+          <div className="grid gap-2">
+            <span className="text-[#2b2b2b] lg:block hidden">D</span>
+            <input
+              type="text"
+              placeholder="Size"
+              className="text-black p-3 rounded-md focus:outline-none"
+            />
+          </div>
+          <div className="grid gap-2">
+            <input
+              type="text"
+              placeholder="Quantity"
+              className="text-black p-3 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
