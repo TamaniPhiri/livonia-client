cecaaffc01b60bbbec5cb6de22609423543fb094
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 15:36:33 +0200

    chnages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div  className="flex items-center relative justify-center w-full flex-col min-h-screen px-4 md:px-8 lg:px-12 py-32">Dashboard</div>
   )
 }
 
