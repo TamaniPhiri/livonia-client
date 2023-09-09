12c986b3b485ddda0442a2d157dd1aeaabe2f992
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 15:17:47 +0200

    chnages

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -3,12 +3,14 @@ const AdminLogin = () => {
     <div className="flex items-center relative justify-center w-full flex-col min-h-screen px-4 md:px-8 lg:px-12 py-32">
       <div className="bg-[#2b2b2b] flex flex-col w-full md:max-w-md p-4 rounded-lg md:p-8">
         <div className="grid gap-2">
-        <span>Name</span>
-        <input type="text" />
+          <span>Name</span>
+          <input type="text" />
         </div>
 
-        <span>Name</span>
-        <input type="text" />
+        <div className="grid gap-2">
+          <span>Name</span>
+          <input type="text" />
+        </div>
       </div>
     </div>
   );
