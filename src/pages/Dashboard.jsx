8d4c1177113fdc46605d9e45479e64b6989eeb43
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 16:11:38 +0200

    chnages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,9 +2,9 @@ import SideBar from "../components/SideBar";
 
 const Dashboard = () => {
   return (
-    <div className="items-center relative justify-center w-full min-h-screen grid grid-cols-6">
+    <div className="items-center relative justify-center w-full min-h-screen grid grid-cols-8">
       <SideBar />
-      <div className=" col-span-5">hello</div>
+      <div className=" col-span-7">hello</div>
     </div>
   );
 };
