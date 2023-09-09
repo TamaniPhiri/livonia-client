f1dd3012b0d1f42aba7a5fd7b284d795cb6f6da8
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 15:23:09 +0200

    chnages

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -12,6 +12,9 @@ const AdminLogin = () => {
           <span className="font-semibold">Password</span>
           <input type="password" placeholder="Password" className="text-black p-3 rounded-md focus:outline-none"/>
         </div>
+        <button className="bg-blue-500 p-3 rounded-md">
+            Login
+        </button>
       </div>
     </div>
   );
