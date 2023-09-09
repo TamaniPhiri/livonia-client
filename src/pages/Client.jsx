9f2281ea027790fb17b6dc6d903736df557e695b
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 12:52:28 +0200

    chnages

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -7,7 +7,7 @@ const Client = () => {
        <div className="flex gap-2 w-full md:max-w-md rounded-full overflow-hidden lg:max-w-lg bg-white items-center justify-center mt-4">
         <input type="text" className="flex-1 p-3 text-black focus:outline-none bg-transparent" placeholder="Search for clients"/>
           <button className="text-black p-3 rounded-full hover:bg-gray-300 m-1 transition-all transform duration-200">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </button>
