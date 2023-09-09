26991e1cb8106b4f14f49601b426a1dd31e24e6d
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 15:21:59 +0200

    chnages

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -4,12 +4,12 @@ const AdminLogin = () => {
       <div className="bg-[#2b2b2b] gap-3 flex flex-col w-full md:max-w-md p-4 rounded-lg md:p-8">
         <h1 className="text-xl font-bold">Admin Login</h1>
         <div className="grid gap-2">
-          <span>Name</span>
+          <span className="font-semibold">Name</span>
           <input type="text" placeholder="User Name" className="text-black p-3 rounded-md focus:outline-none"/>
         </div>
 
         <div className="grid gap-2">
-          <span>Name</span>
+          <span className="font-semibold">Name</span>
           <input type="password" placeholder="Password" className="text-black p-3 rounded-md focus:outline-none"/>
         </div>
       </div>
