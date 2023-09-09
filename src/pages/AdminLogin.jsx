d806ea371b9abff8233fd9ce7c3895e7c2bdbc79
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-09 14:58:14 +0200

    chnages

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,12 +1,12 @@
 const AdminLogin = () => {
   return (
     <div className="min-h-screen py-32 w-full items-center justify-center grid">
-        <div className="bg-[#2b2b2b] max-w-md grid p4 md:p-8">
-            <span>Name</span>
-            <input type="text" />
-        </div>
+      <div className="bg-[#2b2b2b] max-w-lg grid p4 rounded-lg md:p-8">
+        <span>Name</span>
+        <input type="text" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLogin
+export default AdminLogin;
