5b55dd9067179bb48f7915f9af0fae01cac135fa
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-10 17:03:40 +0200

    changes

--- src/pages/PaymentTracking.jsx
+++ src/pages/PaymentTracking.jsx
@@ -5,28 +5,15 @@ const PaymentTracking = () => {
         Payment Tracking
       </h1>
       <div className="border-4 border-gray-700 mt-4 gap-4 p-4 md:p-8 lg:p-12 rounded-3xl flex flex-col items-center justify-center lg:max-w-2xl md:max-w-lg w-full bg-[#2b2b2b]">
-        {/* Client Name and Date of Purchase */}
-        <div className="grid gap-6 lg:grid-cols-2 w-full">
-          {/* Client name */}
-          <div className="grid gap-2">
-            <span>Client Name</span>
-            <input
-              type="text"
-              placeholder="Name"
-              className=" p-3 rounded-md text-black focus:outline-none"
-            />
-          </div>
-
-          {/* Date name */}
-          <div className="grid gap-2">
-            <span>Date of Purchase</span>
-            <input
-              type="date"
-              className="text-black p-3 rounded-md focus:outline-none"
-            />
-          </div>
+        {/* Client name */}
+        <div className="grid gap-2">
+          <span>Client Name</span>
+          <input
+            type="text"
+            placeholder="Name"
+            className=" p-3 rounded-md text-black focus:outline-none"
+          />
         </div>
-
         <h1 className="col-span-2 w-full mt-4 font-semibold text-left">
           Product Details
         </h1>
