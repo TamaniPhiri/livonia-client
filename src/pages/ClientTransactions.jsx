9423fc44f4e3bda664f4c2d364d45a5a2e3ab504
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-10 17:59:29 +0200

    chnages

--- src/pages/ClientTransactions.jsx
+++ src/pages/ClientTransactions.jsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ClientTransactions = () => {
   const { id } = useParams();
   console.log(id);
   const [transactions, setTransactions] = useState([]);
-  return <div>ClientTransactions</div>;
+  useEffect(() => {
+    const getTransactions = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/transaction/client/${id}`
+        );
+        if (response.status === 200) {
+          setTransactions(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getTransactions();
+  }, [id]);
+  return (
+    <div>
+      <div>
+        {transactions.map((item, index) => (
+          <div key={index}>{item.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ClientTransactions;
