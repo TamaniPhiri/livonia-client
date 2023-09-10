0e929bbd7fd688bf036f1aad2dbf36e83f6256ef
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-10 17:52:09 +0200

    chnages

--- src/pages/ClientTransactions.jsx
+++ src/pages/ClientTransactions.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ClientTransactions = () => {
   const { id } = useParams();
   console.log(id);
+  const[transactions,setTransactions]=useState([]);
   return <div>ClientTransactions</div>;
 };
 
