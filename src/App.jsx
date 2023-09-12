e2508df01635c69be927c6498d3b010f41da4801
Tamani Phiri <tamanigabriel0@gmail.com>
2023-09-12 13:44:45 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Dashboard from "./pages/Dashboard";
 import Inventory from "./pages/Inventory";
 import Client from "./pages/Client";
 import PaymentTracking from "./pages/PaymentTracking";
 import Navbar from "./components/Global/Navbar";
 import Login from "./pages/Login";
-import AdminLogin from "./pages/AdminLogin";
 import ClientTransactions from "./pages/ClientTransactions";
 
 function App() {
@@ -14,10 +12,8 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" Component={AdminLogin} />
-        <Route path="/dashboard/*" Component={Dashboard} />
+      <Route path="/" Component={Client} />
         <Route path="/inventory" Component={Inventory} />
-        <Route path="/client" Component={Client} />
         <Route path="/client/transaction/:id" Component={ClientTransactions}/>
         <Route path="/payment-tracking" Component={PaymentTracking} />
         <Route path="/login" Component={Login} />
